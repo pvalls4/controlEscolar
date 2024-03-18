2e421db974aa81ecf4785cb4a36f0a03aa04b918
pvalls4 <valls.vilalta.4@gmail.com>
2024-03-18 13:06:04 +0100

    1.1

--- src/main/java/main/controlescolar/JakartaRestConfiguration.java
+++ src/main/java/main/controlescolar/JakartaRestConfiguration.java
@@ -1,4 +1,4 @@
-package valls.controlescolar;
+package main.controlescolar;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
--- src/main/java/main/controlescolar/resources/JakartaEE10Resource.java
+++ src/main/java/main/controlescolar/resources/JakartaEE10Resource.java
@@ -1,4 +1,4 @@
-package valls.controlescolar.resources;
+package main.controlescolar.resources;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
